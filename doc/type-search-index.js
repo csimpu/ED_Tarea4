6d0b0f514423b3a1ba208a99667cfedb2704475b
Borja ThePortableOne <borja.pinero@gmail.com>
2025-02-16 21:27:51 +0100

    añadido javadoc a doc/

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"damel.entornos.cuentas","l":"CCuenta"},{"p":"damel.entornos.cuentas","l":"Main"}];updateSearchResults();

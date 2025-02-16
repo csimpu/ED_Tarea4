4ae1af564ec0539f360638a7f7433fd080a1052c
Borja ThePortableOne <borja.pinero@gmail.com>
2025-02-16 21:40:40 +0100

    eliminar doc/

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"damel.entornos.cuentas","l":"CCuenta"},{"p":"damel.entornos.cuentas","l":"Main"}];updateSearchResults();
